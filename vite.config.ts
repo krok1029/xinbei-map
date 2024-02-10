7e5a777991dd8b102c9469557dbfbdd531173950
leo lee <krok1029@gmail.com>
2024-02-10 14:11:13 +0800

    enviorment setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: "/xinbei-map/",
-});
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+})
